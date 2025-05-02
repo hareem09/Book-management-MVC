11a9d170968a4635194be39eda002afef3a5ff2b
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-05-02 17:15:04 +0500

    Update bookSchema.js

--- model/bookSchema.js
+++ model/bookSchema.js
@@ -3,9 +3,18 @@ const mongodb=require('mongoose');
 mongodb.connect('mongodb://localhost:27017/bookDataBase');
 
 let dataSchema=new mongodb.Schema({
-    title:String,
-    author:String,
-    year:Number
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type:String,
+        required: true
+    },
+    year:{
+        type:Number,
+        required: true
+    }
 })
 
 module.exports = mongodb.model('data',dataSchema);
